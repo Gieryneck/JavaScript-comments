8e91d1306a374551b0789cd459bd57c8adb74ac9
Gieryneck <gierynt@gmail.com>
2017-10-03 19:05:21 +0200

    Created the repo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//script.js
+
+var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+console.log('Hello ' + name);
+
+/* 
+
+var nazwa_zmiennej = 'test';
+console.log(nazwa_zmiennej); 
+
+Konsola wyświetli aktualną wartość zmiennej, 
+dzieki temu mozemy uniknac bledu gdy wydaje
+sie nam ze zmienna ma w danej chwili jakas inna wartosc.
+
+*/
+
+// konsola na windowsie: ctrl+shift+i
